545ec6fe794865a5ee3061a59b519e7f53c6dc9e
Chris Sewell <chrisj_sewell@hotmail.com>
2017-06-07 17:58:00 +0100

    update version for pipit test

--- pandas3js/__init__.py
+++ pandas3js/__init__.py
@@ -5,7 +5,7 @@
 
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from pandas3js.models.idobject import (IDObject, GeometricObject, 
                                         Sphere, WireBox, Line)

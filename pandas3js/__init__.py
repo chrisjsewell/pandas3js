41950e723a77288b59bd015cff8e5437d78861e2
Chris Sewell <chrisj_sewell@hotmail.com>
2017-06-07 17:12:04 +0100

    updating version

--- pandas3js/__init__.py
+++ pandas3js/__init__.py
@@ -5,7 +5,7 @@
 
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from pandas3js.models.idobject import (IDObject, GeometricObject, 
                                         Sphere, WireBox, Line)
